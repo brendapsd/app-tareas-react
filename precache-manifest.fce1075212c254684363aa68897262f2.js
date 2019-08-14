79592f59eadef90c6d5fcd75a1f6f40b6dcb1395
brendapsd <brenda.sd.15@gmail.com>
2019-08-14 18:58:20 -0500

    Updates

---
+++ precache-manifest.fce1075212c254684363aa68897262f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "64a5ed6017d923b07eb4",
+    "url": "/app-tareas-react/static/js/runtime~main.64a5ed60.js"
+  },
+  {
+    "revision": "d6b80ef92f196b027571",
+    "url": "/app-tareas-react/static/js/main.d6b80ef9.chunk.js"
+  },
+  {
+    "revision": "0ff4267af7a99a6a6eb3",
+    "url": "/app-tareas-react/static/js/1.0ff4267a.chunk.js"
+  },
+  {
+    "revision": "d6b80ef92f196b027571",
+    "url": "/app-tareas-react/static/css/main.903079e1.chunk.css"
+  },
+  {
+    "revision": "c5c1a6ef2221f1e0a88a13edb642107b",
+    "url": "/app-tareas-react/index.html"
+  }
+];
